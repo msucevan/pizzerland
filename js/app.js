327d7f2566470b2e3d2b00e1f2d8b6af260a8f49
msucevan <marius.sucevan@gmail.com>
2021-04-21 12:44:05 +0200

    Final style + Animation

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById('cta-btn');
+let overlay = document.getElementById('overlay');
+
+btn.addEventListener('click', () => {;
+    overlay.style.display = 'grid';
+    overlay.classList.add('animate-overlay')
+    setTimeout(function() {
+
+        window.location.reload()
+    }, 3000);
+})
